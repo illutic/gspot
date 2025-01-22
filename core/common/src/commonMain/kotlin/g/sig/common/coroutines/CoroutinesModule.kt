d217ee4f653edd44a3a53a18f2d8b44e959c0137
George Sigalas <gsigalas@skroutz.gr>
2025-01-22 13:12:40 +0200

    fix: Remove Io Dispatcher

--- core/common/src/commonMain/kotlin/g/sig/common/coroutines/CoroutinesModule.kt
+++ core/common/src/commonMain/kotlin/g/sig/common/coroutines/CoroutinesModule.kt
@@ -2,16 +2,12 @@ package g.sig.common.coroutines
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
-private const val IO_DISPATCHER = "ioDispatcher"
 private const val MAIN_DISPATCHER = "mainDispatcher"
 private const val DEFAULT_DISPATCHER = "defaultDispatcher"
-
-fun Scope.provideIoDispatcher(): CoroutineDispatcher = get(named(IO_DISPATCHER))
 
 fun Scope.provideMainDispatcher(): CoroutineDispatcher = get(named(MAIN_DISPATCHER))
 
@@ -19,10 +15,6 @@ fun Scope.provideDefaultDispatcher(): CoroutineDispatcher = get(named(DEFAULT_DI
 
 val coroutinesModule =
     module {
-        single(named(IO_DISPATCHER)) {
-            Dispatchers.IO
-        }
-
         single(named(MAIN_DISPATCHER)) {
             Dispatchers.Main
         }
